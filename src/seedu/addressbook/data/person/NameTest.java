a13345c6a05ebc718a6bc1549845a23684598884
Bernard <bernardified@gmail.com>
2016-09-07 00:05:36 +0800

    Add isSimilar method in Name class to return false if argument is null

--- src/seedu/addressbook/data/person/NameTest.java
+++ src/seedu/addressbook/data/person/NameTest.java
@@ -4,14 +4,17 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import seedu.addressbook.data.exception.IllegalValueException;
+
 public class NameTest {
 
 	/**
 	 * Test isSimilar method in Name class
+	 * @throws IllegalValueException 
 	 */
 	
 	@Test
-	public void isSimilar_null_returnsFalse() {
+	public void isSimilar_null_returnsFalse() throws IllegalValueException {
 		Name testNull = new Name("John Doe");
 		assertFalse("null input should return false", testNull.isSimilar(null));
 	}
